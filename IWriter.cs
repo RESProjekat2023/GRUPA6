978f459858f63a8ca5f0e293a901d71f2b0bfb21
BojanSkopljakk <122480134+BojanSkopljakk@users.noreply.github.com>
2023-01-18 17:51:53 +0100

    Add files via upload
    Klasa IWriter.cs

---
+++ IWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekat.writer
+{
+    public interface IWriter
+    {
+        string LogMsg(String str);
+        void Write(int workerID);
+        void WriteManualy(int workerID, int dataSetID, List<WriterData> data, int descriptionID);
+        void TurnOnWorker(Worker w);
+        void TurnOffWorker(Worker w);
+    }
+}
