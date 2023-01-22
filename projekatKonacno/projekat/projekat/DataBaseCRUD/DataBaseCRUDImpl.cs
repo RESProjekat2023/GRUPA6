5b2be51f66e710e17acac84816a62f4086758e53
AleksandarAntonijev <antonijev.pr109.2020@uns.ac.rs>
2023-01-22 14:00:30 +0100

    Prosirenje DataBaseCRUD komponente

--- projekatKonacno/projekat/projekat/DataBaseCRUD/DataBaseCRUDImpl.cs
+++ projekatKonacno/projekat/projekat/DataBaseCRUD/DataBaseCRUDImpl.cs
@@ -71,6 +71,36 @@ namespace projekat.dataBaseCRUD
                     command.ExecuteNonQuery();
                 }
             }
+
+        public void Delete(int id_brojila)
+        {
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM brojilo WHERE id_brojila = @id_brojila";
+                command.Parameters.AddWithValue("@id_brojila", id_brojila);
+                command.ExecuteNonQuery();
+            }
         }
+
+
+        public void Update(int id_brojila, string ime_korisnika, string prezime_korisnika, string ulica, int broj, int postanski_broj, string grad)
+        {
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "UPDATE brojilo SET ime_korisnika = @ime_korisnika, prezime_korisnika = @prezime_korisnika, ulica = @ulica, broj = @broj, postanski_broj = @postanski_broj, grad = @grad WHERE id_brojila = @id_brojila";
+                command.Parameters.AddWithValue("@ime_korisnika", ime_korisnika);
+                command.Parameters.AddWithValue("@prezime_korisnika", prezime_korisnika);
+                command.Parameters.AddWithValue("@ulica", ulica);
+                command.Parameters.AddWithValue("@broj", broj);
+                command.Parameters.AddWithValue("@postanski_broj", postanski_broj);
+                command.Parameters.AddWithValue("@grad", grad);
+                command.Parameters.AddWithValue("@id_brojila", id_brojila);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
 }
+
 
--- projekatKonacno/projekat/projekat/DataBaseCRUD/IDataBaseCRUD.cs
+++ projekatKonacno/projekat/projekat/DataBaseCRUD/IDataBaseCRUD.cs
@@ -17,6 +17,8 @@ namespace projekat.dataBaseCRUD
         int Count();
         void Insert();
 
+        void Update(int id_brojila, string ime_korisnika, string prezime_korisnika, string ulica, int broj, int postanski_broj, string grad);
+        void Delete(int id_brojila);
 
 
     }   
